3b7222fcb286704c88b0229a402eb4b70ea6fd8c
filip <filip.pandi@inovatrend.com>
2023-04-14 09:15:15 +0200

    Init

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+    return (
+        <div>
+            CHILD 1!
+        </div>
+    );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+window.renderChild1 = (containerId, history) => {
+    console.log(containerId);
+
+    ReactDOM.render(
+        <App history={history}/>,
+        document.getElementById(containerId),
+    );
+};
+
+window.unmountChild1 = containerId => {
+    ReactDOM.unmountComponentAtNode(document.getElementById(containerId));
+};
+
+if (!document.getElementById('Child1-container')) {
+    ReactDOM.render(<App/>, document.getElementById('root'));
+}
